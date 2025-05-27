0ff4b0fac9c934c1b2194b2f932a75d325e7b01e
FlippFlopp89 <jackbjermarker@gmail.com>
2025-05-27 05:47:03 +0200

    Update client.js

--- client.js
+++ client.js
@@ -4,11 +4,7 @@ window.TrelloPowerUp.initialize({
       icon: 'https://cdn-icons-png.flaticon.com/512/61/61456.png',
       text: 'Open Weekly Grid',
       callback: function(t) {
-        return t.popup({
-          title: 'Weekly Grid',
-          url: 'index.html',
-          height: 600
-        });
+        alert('Grid would open now.');
       }
     }];
   }
